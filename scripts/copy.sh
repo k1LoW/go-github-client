70953c15b6c07d5e229d45c8a7e567193209960a
k1LoW <k1lowxb@gmail.com>
2022-02-07 10:13:57 +0900

    bonsai

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -9,6 +9,7 @@ cp -r $src $dest
 rm $dest/go.mod $dest/go.sum
 find $dest -type f | xargs sed -i -e "s#k1LoW/go-github-client/$src#k1LoW/go-github-client/$dest#g"
 find $dest -type f | xargs sed -i -e "s#google/go-github/$src#google/go-github/$dest#g"
+find $dest -type f | grep -e '-e' | xargs rm
 cd $dest
 echo "module \"$(pwd | sed -e 's/.*\/src\///')\"" > go.mod
 go mod tidy
