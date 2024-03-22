f618e88f41bd3d6317497697a14e1f3cbe19bb35
k1LoW <k1lowxb@gmail.com>
2024-03-22 16:24:02 +0900

    Update v60

---
+++ v60/factory/factory_test.go
@@ -0,0 +1,195 @@
+package factory
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/cli/go-gh/v2/pkg/config"
+	"github.com/google/go-github/v60/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+)
+
+func TestGetTokenAndEndpoints(t *testing.T) {
+	t.Setenv("GH_CONFIG_DIR", filepath.Join(testdataDir(t), "config"))
+	// set config
+	// ref: https://github.com/cli/go-gh/blob/98bbeb261673e1c506e965ab1553bfbaf5318250/pkg/config/config.go#L124-L128
+	_, _ = config.Read()
+
+	tests := []struct {
+		GH_HOST                 string
+		GH_ENTERPRISE_TOKEN     string
+		GITHUB_ENTERPRISE_TOKEN string
+		GH_TOKEN                string
+		GITHUB_TOKEN            string
+		GITHUB_API_URL          string
+		wantToken               string
+		wantEndpoint            string
+		wantUploadEndpoint      string
+		wantV4Endpoint          string
+	}{
+		{"", "", "", "", "", "", "gho_XXXXXxxxxXXXXxxxXXXXXX", "https://api.github.com", "https://uploads.github.com", "https://api.github.com/graphql"},
+		{"git.example.com", "", "", "", "", "", "", "https://git.example.com/api/v3", "https://git.example.com/api/uploads", "https://git.example.com/api/graphql"},
+		{"git.example.com", "GH_ENTERPRISE_TOKEN", "", "", "", "", "GH_ENTERPRISE_TOKEN", "https://git.example.com/api/v3", "https://git.example.com/api/uploads", "https://git.example.com/api/graphql"},
+		{"git.example.com", "GH_ENTERPRISE_TOKEN", "GITHUB_ENTERPRISE_TOKEN", "", "", "", "GH_ENTERPRISE_TOKEN", "https://git.example.com/api/v3", "https://git.example.com/api/uploads", "https://git.example.com/api/graphql"},
+		{"git.example.com", "", "GITHUB_ENTERPRISE_TOKEN", "", "", "", "GITHUB_ENTERPRISE_TOKEN", "https://git.example.com/api/v3", "https://git.example.com/api/uploads", "https://git.example.com/api/graphql"},
+		{"github.com", "GH_ENTERPRISE_TOKEN", "", "", "", "", "gho_XXXXXxxxxXXXXxxxXXXXXX", "https://api.github.com", "https://uploads.github.com", "https://api.github.com/graphql"},
+		{"", "", "", "GH_TOKEN", "", "", "GH_TOKEN", "https://api.github.com", "https://uploads.github.com", "https://api.github.com/graphql"},
+		{"", "", "", "", "GITHUB_TOKEN", "", "GITHUB_TOKEN", "https://api.github.com", "https://uploads.github.com", "https://api.github.com/graphql"},
+		{"", "", "", "", "", "GITHUB_API_URL", "gho_XXXXXxxxxXXXXxxxXXXXXX", "GITHUB_API_URL", "https://uploads.github.com", "https://api.github.com/graphql"},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Setenv("GH_HOST", tt.GH_HOST)
+			t.Setenv("GH_ENTERPRISE_TOKEN", tt.GH_ENTERPRISE_TOKEN)
+			t.Setenv("GITHUB_ENTERPRISE_TOKEN", tt.GITHUB_ENTERPRISE_TOKEN)
+			t.Setenv("GH_TOKEN", tt.GH_TOKEN)
+			t.Setenv("GITHUB_TOKEN", tt.GITHUB_TOKEN)
+			t.Setenv("GITHUB_API_URL", tt.GITHUB_API_URL)
+			gotToken, gotEndpoint, gotUploadEndpoint, gotV4Endpoint := GetTokenAndEndpoints()
+
+			if gotToken != tt.wantToken {
+				t.Errorf("got %v\nwant %v", gotToken, tt.wantToken)
+			}
+
+			if gotEndpoint != tt.wantEndpoint {
+				t.Errorf("got %v\nwant %v", gotEndpoint, tt.wantEndpoint)
+			}
+
+			if gotUploadEndpoint != tt.wantUploadEndpoint {
+				t.Errorf("got %v\nwant %v", gotUploadEndpoint, tt.wantUploadEndpoint)
+			}
+
+			if gotV4Endpoint != tt.wantV4Endpoint {
+				t.Errorf("got %v\nwant %v", gotV4Endpoint, tt.wantV4Endpoint)
+			}
+		})
+	}
+}
+
+func TestEndpoint(t *testing.T) {
+	tests := []struct {
+		GH_HOST                 string
+		GH_ENTERPRISE_TOKEN     string
+		GITHUB_ENTERPRISE_TOKEN string
+		GH_TOKEN                string
+		GITHUB_API_URL          string
+		wantEndpoint            string
+		wantUploadURL           string
+	}{
+		{"", "", "", "", "", "https://api.github.com/", "https://uploads.github.com/"},
+		{"git.example.com", "GH_ENTERPRISE_TOKEN", "", "", "", "https://git.example.com/api/v3/", "https://git.example.com/api/uploads/"},
+		{"", "", "", "", "https://git.example.com/api/v3", "https://git.example.com/api/v3/", "https://git.example.com/api/uploads/"},
+		{"", "", "", "", "https://api.github.com", "https://api.github.com/", "https://uploads.github.com/"},
+	}
+	t.Setenv("GITHUB_TOKEN", "GITHUB_TOKEN")
+
+	for _, tt := range tests {
+		t.Setenv("GH_HOST", tt.GH_HOST)
+		t.Setenv("GH_ENTERPRISE_TOKEN", tt.GH_ENTERPRISE_TOKEN)
+		t.Setenv("GITHUB_ENTERPRISE_TOKEN", tt.GITHUB_ENTERPRISE_TOKEN)
+		t.Setenv("GH_TOKEN", tt.GH_TOKEN)
+		t.Setenv("GITHUB_API_URL", tt.GITHUB_API_URL)
+
+		client, err := NewGithubClient()
+		if err != nil {
+			t.Fatal(err)
+			continue
+		}
+
+		gotEndpoint := client.BaseURL.String()
+		gotUploadURL := client.UploadURL.String()
+
+		if gotEndpoint != tt.wantEndpoint {
+			t.Errorf("got %v\nwant %v", gotEndpoint, tt.wantEndpoint)
+		}
+
+		if gotUploadURL != tt.wantUploadURL {
+			t.Errorf("got %v\nwant %v", gotUploadURL, tt.wantUploadURL)
+		}
+	}
+}
+
+func TestNewGithubClient(t *testing.T) {
+	_, err := NewGithubClient()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestNewGithubClientUsingMock(t *testing.T) {
+	mockedHTTPClient := mock.NewMockedHTTPClient(
+		mock.WithRequestMatch(
+			mock.GetUsersByUsername,
+			github.User{
+				Name: github.String("foobar"),
+			},
+		),
+	)
+	c, err := NewGithubClient(HTTPClient(mockedHTTPClient))
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx := context.Background()
+	user, _, err := c.Users.Get(ctx, "foobar")
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := user.GetName()
+	if want := "foobar"; got != want {
+		t.Errorf("got %v\nwant %v", got, want)
+	}
+}
+
+func TestDetectOwnerRepo(t *testing.T) {
+	tests := []struct {
+		GH_REPO                 string
+		GITHUB_REPOSITORY       string
+		GITHUB_REPOSITORY_OWNER string
+		wantOwner               string
+		wantRepo                string
+		wantErr                 bool
+	}{
+		{"", "", "", "", "", true},
+		{"example/myrepo", "", "", "example", "myrepo", false},
+		{"git.example.com/example/myrepo", "", "", "example", "myrepo", false},
+		{"", "example/myrepo", "", "example", "myrepo", false},
+		{"example/ourrepo", "example/myrepo", "", "example", "ourrepo", false},
+		{"", "", "example", "example", "", false},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Setenv("GH_REPO", tt.GH_REPO)
+			t.Setenv("GITHUB_REPOSITORY", tt.GITHUB_REPOSITORY)
+			t.Setenv("GITHUB_REPOSITORY_OWNER", tt.GITHUB_REPOSITORY_OWNER)
+			gotOwner, gotRepo, err := detectOwnerRepo()
+			if err != nil {
+				if !tt.wantErr {
+					t.Errorf("got error: %v", err)
+				}
+				return
+			}
+			if gotOwner != tt.wantOwner {
+				t.Errorf("got %v\nwant %v", gotOwner, tt.wantOwner)
+			}
+			if gotRepo != tt.wantRepo {
+				t.Errorf("got %v\nwant %v", gotRepo, tt.wantRepo)
+			}
+		})
+	}
+}
+
+func testdataDir(t *testing.T) string {
+	t.Helper()
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	dir, err := filepath.Abs(filepath.Join(filepath.Dir(wd), "testdata"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	return dir
+}
---
+++ v60/go.mod
@@ -0,0 +1,35 @@
+module github.com/k1LoW/go-github-client/v60
+
+go 1.22.1
+
+require (
+	github.com/bradleyfalzon/ghinstallation/v2 v2.10.0
+	github.com/cli/go-gh/v2 v2.6.0
+	github.com/google/go-github/v60 v60.0.0
+	github.com/k1LoW/httpstub v0.15.1
+	github.com/migueleliasweb/go-github-mock v0.0.23
+)
+
+require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/cli/safeexec v1.0.0 // indirect
+	github.com/dprotaso/go-yit v0.0.0-20220510233725-9ba8df137936 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/google/go-github/v59 v59.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/minio/pkg v1.7.5 // indirect
+	github.com/pb33f/libopenapi v0.15.14 // indirect
+	github.com/pb33f/libopenapi-validator v0.0.44 // indirect
+	github.com/santhosh-tekuri/jsonschema/v5 v5.3.1 // indirect
+	github.com/vmware-labs/yaml-jsonpath v0.3.2 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	golang.org/x/exp v0.0.0-20240213143201-ec583247a57a // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
