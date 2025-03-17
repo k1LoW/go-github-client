35b0fa37aa59ddac78cd3d6351ffcc8164bf2c7a
k1LoW <k1lowxb@gmail.com>
2025-03-17 23:30:14 +0900

    chore: update patch

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,7 +2,7 @@
 
 src=$1
 dest=$2
-patch=13
+patch=14
 
 rm -rf $dest
 cp -r $src $dest
@@ -12,7 +12,6 @@ find $dest -type f | xargs sed -i -e "s#google/go-github/$src#google/go-github/$
 find $dest -type f | grep -e '-e' | xargs rm
 cd $dest
 echo "module \"$(pwd | sed -e 's/.*\/src\///')\"" > go.mod
-go get github.com/migueleliasweb/go-github-mock@v1.0.1
 go mod tidy
 go get -u golang.org/x/crypto
 go get -u github.com/cloudflare/circl
